ed249d8bf1ce863c9b67e28d55cd2467c848cecf
John Wiegley <johnw@newartisans.com>
2012-08-25 00:57:33 -0500

    Move text munging from Makefile to literate

--- Main.hs
+++ Main.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import System.Console.CmdArgs
 import Control.Arrow
 import Text.BlogLiterately
 
-import Text.Pandoc (Pandoc(..), Block (RawBlock))
+import qualified System.IO.UTF8 as U (readFile)
+import Text.Pandoc
 import Data.List
+import Text.Blaze.Html.Renderer.String (renderHtml)
 
+replace :: (Eq a) => [a] -> [a] -> [a] -> [a]
+replace [] _ _ = []
+replace l@(x:xs) i j = if i `isPrefixOf` l
+                       then j ++ replace (drop (length i) l) i j
+                       else x : replace xs i j
+
 debirdXF :: Transform
 debirdXF = Transform (const (arr removeAllTracks)) (const True)
 
@@ -11,24 +22,67 @@ removeAllTracks :: Pandoc -> Pandoc
 removeAllTracks (Pandoc m blocks) =
     Pandoc m $ map removeBirdTracks blocks
 
-replace :: (Eq a) => [a] -> [a] -> [a] -> [a]
-replace [] _ _ = []
-replace l@(x:xs) i j = if i `isPrefixOf` l
-                       then j ++ replace (drop (length i) l) i j
-                       else x : replace xs i j
-
 removeBirdTracks :: Block -> Block
 removeBirdTracks (RawBlock "html" src) =
   let src'  = (replace src "<span style=\"\">&gt;</span>"
                        "<span style=\"\"></span>")
   in RawBlock "html" src'
 removeBirdTracks b = b
+
+hidecodeXF :: Transform
+hidecodeXF = Transform (const (arr hideCodeBlocks)) (const True)
+
+hideCodeBlocks :: Pandoc -> Pandoc
+hideCodeBlocks (Pandoc m blocks) =
+    Pandoc m $ map hiddenBlocks blocks
+
+hiddenBlocks :: Block -> Block
+hiddenBlocks b@(CodeBlock _ src) =
+  if "-- HIDE" `isInfixOf` src then Null else b
+hiddenBlocks b = b
+
+fixHeaders :: String -> String
+fixHeaders [] = []
+fixHeaders ('\\':'#':cs) = '#':cs
+fixHeaders (c:cs) = c:fixHeaders cs
+
+xformDoc' :: BlogLiterately -> [Transform] -> (String -> IO String)
+xformDoc' bl xforms = runKleisli $
+       arr     fixHeaders
+    >>> arr     (readMarkdown parseOpts)
+
+    >>> runTransforms xforms bl
+
+    >>> arr     (writeHtml writeOpts)
+    >>> arr     renderHtml
+  where
+    writeOpts = defaultWriterOptions
+                { writerReferenceLinks = True }
+    parseOpts = defaultParserState
+                { stateLiterateHaskell = True }
+
+blogLiteratelyCustom' :: [Transform] -> IO ()
+blogLiteratelyCustom' ts = do
+    bl <- cmdArgs blOpts
+    let (BlogLiterately{..}) = bl
+
+    prefs <- getStylePrefs style
+    let hsHighlight' = case hsHighlight of
+            HsColourInline _ -> HsColourInline prefs
+            _                -> hsHighlight
+        bl' = bl { hsHighlight = hsHighlight' }
+
+    let bl'' = bl' { password = password }
+
+    html <- xformDoc' bl'' ts =<< U.readFile file
+    postIt bl'' html
 
 main :: IO ()
-main = blogLiteratelyCustom [ ghciXF
-                            , imagesXF
-                            , highlightXF
-                            , debirdXF
-                            , centerImagesXF ]
+main = blogLiteratelyCustom' [ ghciXF
+                             , imagesXF
+                             , hidecodeXF
+                             , highlightXF
+                             , debirdXF
+                             , centerImagesXF ]
 
 -- literate.hs ends here
--- Makefile
+++ Makefile
@@ -13,39 +13,15 @@ clean:
 	rm $(DEST)
 
 %.html: %.lhs
-	cat $<						\
-	    | perl -ne 'print unless /-- HIDE/'		\
-	    | perl -pe 's/^\\#/#/;'			\
-	    > /tmp/$<
-	literate --ghci /tmp/$< > $@
-	@rm -f /tmp/$<
+	literate --ghci $< > $@
 
 upload: $(SOURCE)
-	cat $<						\
-	    | perl -ne 'print unless /-- HIDE/'		\
-	    | perl -pe 's/^\\#/#/;'			\
-	    > /tmp/$<
 	literate --ghci --upload-images					\
 	         --blog http://$(HOST)/xmlrpc.php			\
 	         --user "$(USER)" --password "$(PASS)"			\
                  --title "$(shell head -1 info)"			\
 		 --postid $(shell tail -1 info)				\
 		 --category $(shell head -2 info | tail -1)		\
-	         /tmp/$<
-	@rm -f /tmp/$<
-
-publish: $(SOURCE)
-	cat $<						\
-	    | perl -ne 'print unless /-- HIDE/'		\
-	    | perl -pe 's/^\\#/#/;'			\
-	    > /tmp/$<
-	literate --ghci --upload-images --publish			\
-	         --blog http://$(HOST)/xmlrpc.php			\
-	         --user "$(USER)" --password "$(PASS)"			\
-                 --title "$(shell head -1 info)"			\
-		 --postid $(shell tail -1 info)				\
-		 --category $(shell head -2 info | tail -1)		\
-	         /tmp/$<
-	@rm -f /tmp/$<
+	         $<
 
 ### Makefile ends here
